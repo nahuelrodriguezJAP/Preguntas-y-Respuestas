1c78042e9688a9879eba8aaffeddc7416a068003
Nahuel <Soccernahu@gmail.com>
2022-05-27 16:38:42 -0300

    Funcionamiento game

--- game.js
+++ game.js
@@ -1,18 +1,31 @@
 
-var nivelActual=0
+var nivelActual = 0
 function aleatorio(max, min) {
     return Math.round(Math.random() * (1 + max - min) + min);
 }
 
 function preguntar(niveles) {
-    let preguntas = "";
+    var preguntas = "";
+    var respuestas = [];
     let valorAleatorio = aleatorio(3, 0);
-    preguntas += niveles.nivel+valorAleatorio;
-    console.log(preguntas)
-    document.getElementById('pregunta').innerHTML= preguntas[valorAleatorio].pregunta
-    
+    preguntas = niveles[nivelActual];
+    console.log(preguntas[valorAleatorio])
+    document.getElementById('pregunta').innerText = preguntas[valorAleatorio].pregunta;
+    respuestas[0] = `${preguntas[valorAleatorio].respIncorrecta[0]}`;
+    respuestas[1] = `${preguntas[valorAleatorio].respIncorrecta[1]}`;
+    respuestas[2] = `${preguntas[valorAleatorio].respIncorrecta[2]}`;
+    respuestas[3] = `${preguntas[valorAleatorio].respCorrecta}`;
+    console.log(respuestas);
+    for (let i = 0; i < respuestas.length; i++) {
+        var resp = respuestas[i];
+        console.log(resp)
+        document.getElementById('respuestas').innerHTML += `<div id="${i}"><Button>${resp}`
+    }
+
 }
-function mezclarResp(x,y){
+console.log(respuestas)
+function mezclarResp(x, y) {
+
 
 }
 document.addEventListener("DOMContentLoaded", function (e) {
@@ -20,11 +33,12 @@ document.addEventListener("DOMContentLoaded", function (e) {
         if (result.status === 'ok') {
             var niveles = ""
             niveles = result.data;
-            console.log(result.data.nivel1[1]);
-            
-    preguntar(niveles)
+            console.log(result.data);
+            console.log(result.data[aleatorio(3, 0)])
+
+            preguntar(niveles)
         }
-        
+
     });
 });
 console.log(aleatorio(3, 0))
