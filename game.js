efae766d0a2c7452cf0015246cd3dc9cd89762a3
Nahuel <Soccernahu@gmail.com>
2022-05-27 15:07:15 -0300

    a

--- game.js
+++ game.js
@@ -1,13 +1,30 @@
 
-function Preguntar(niveles){
-    let pregunta = '';
+var nivelActual=0
+function aleatorio(max, min) {
+    return Math.round(Math.random() * (1 + max - min) + min);
+}
+
+function preguntar(niveles) {
+    let preguntas = "";
+    let valorAleatorio = aleatorio(3, 0);
+    preguntas += niveles.nivel+valorAleatorio;
+    console.log(preguntas)
+    document.getElementById('pregunta').innerHTML= preguntas[valorAleatorio].pregunta
+    
+}
+function mezclarResp(x,y){
 
 }
-console.log(niveles)
 document.addEventListener("DOMContentLoaded", function (e) {
-    getJSONData(NIVEL).then(function(result){
+    getJSONData(NIVEL).then(function (result) {
         if (result.status === 'ok') {
-            niveles = result.data
+            var niveles = ""
+            niveles = result.data;
+            console.log(result.data.nivel1[1]);
+            
+    preguntar(niveles)
         }
+        
     });
 });
+console.log(aleatorio(3, 0))
