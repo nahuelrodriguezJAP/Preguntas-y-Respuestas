878cd5191fda55dad14887af9139fd1915a35390
Nahuel <Soccernahu@gmail.com>
2022-05-27 13:04:11 -0300

    first commit

---
+++ init.js
@@ -0,0 +1,21 @@
+var getJSONData = function(url){
+    var result = {};
+    return fetch(url)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }else{
+        throw Error(response.statusText);
+      }
+    })
+    .then(function(response) {
+          result.status = 'ok';
+          result.data = response;
+          return result;
+    })
+    .catch(function(error) {
+        result.status = 'error';
+        result.data = error;
+        return result;
+    });
+}
