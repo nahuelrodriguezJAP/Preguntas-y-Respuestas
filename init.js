3a2d912678c0aa370c88959bc695da7681b224ff
Nahuel <Soccernahu@gmail.com>
2022-05-27 14:22:40 -0300

    Json Final

--- init.js
+++ init.js
@@ -1,11 +1,5 @@
-const NIVELES = ["1","2","3","4","5"];
-const Lvl1="https://nahuelrodriguezjap.github.io/Preguntas-y-Respuestas/preguntas/nivel1.json";
-const Lvl2="https://nahuelrodriguezjap.github.io/Preguntas-y-Respuestas/preguntas/nivel2.json";
-const Lvl3="https://nahuelrodriguezjap.github.io/Preguntas-y-Respuestas/preguntas/nivel3.json";
-const Lvl4="https://nahuelrodriguezjap.github.io/Preguntas-y-Respuestas/preguntas/nivel4.json";
-const Lvl5="https://nahuelrodriguezjap.github.io/Preguntas-y-Respuestas/preguntas/nivel5.json";
-
-
+var NivelActual="";
+const NIVEL="https://nahuelrodriguezjap.github.io/Preguntas-y-Respuestas/preguntas/nivel.json";
 
 
 var getJSONData = function(url){
